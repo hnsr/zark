fae7deb51872393dc201a9329cbf455191c7ba75
Hans Nieser <hnsr@xs4all.nl>
2009-09-30 13:30:41 +0200

    create build-aux dir if missing so automake doesn't error out

--- autogen.sh
+++ autogen.sh
@@ -5,5 +5,11 @@
 aclocal -I m4
 #autoheader
 autoconf
+
+# Prevent automake from erroring out on missing build-aux dir
+if test \! -e build-aux; then
+    mkdir build-aux
+fi
+
 automake --copy --add-missing
 
