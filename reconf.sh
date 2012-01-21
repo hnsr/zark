d37520fda62eb556117a923ac26866e81699f18a
Hans Nieser <hnsr@xs4all.nl>
2012-01-21 11:05:14 +0100

    clean up reconf.sh a bit

--- reconf.sh
+++ reconf.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
+
+# Quick and dirty script to generate and configure build system
+
 ./autogen.sh
 
-CONF="./configure --prefix=/home/hans/dev --with-asset-dir=/home/hans/prog/zark_data"
+# Base configure commandline, modify paths as needed
+CONF="./configure --prefix=/usr/local --with-asset-dir=/usr/local/share/zark-data"
 
-export CFLAGS="-march=athlon64 -O2 -pipe"
+# Base CFLAGS, modify as needed
+BASE_CFLAGS="-march=athlon64 -O2 -pipe"
+
+
+# Uncomment one of these (debugging/release/release+profiling)
 
 # Debugging
-CFLAGS="$CFLAGS -g" $CONF --enable-debug
+#CFLAGS="$BASE_CFLAGS -g" $CONF --enable-debug
 
 # Release
-#$CONF
+CFLAGS="$BASE_CFLAGS" $CONF
 
 # Release + profiling
-#LDFLAGS="-pg" CFLAGS="$CFLAGS -pg" $CONF
+#CFLAGS="$BASE_CFLAGS -pg" LDFLAGS="-pg" $CONF
 
